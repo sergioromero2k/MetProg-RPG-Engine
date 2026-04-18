5ff1551a99aea0ea9cfc3bfa8d29014841e6c81b
Sergio Alejandro <sergioromero2k@gmail.com>
2026-04-18 18:56:37 +0200

    Adds the abstract SpecialAbility class with attributes, a constructor, and getters and setters

--- src/main/java/metprog/model/HabilidadEspecial.java
+++ src/main/java/metprog/model/HabilidadEspecial.java
@@ -1,5 +1,46 @@
 package metprog.model;
 
-public class HabilidadEspecial {
+import java.io.Serializable;
 
+public abstract class HabilidadEspecial  implements Serializable {
+  private String nombre;
+  private int valorAtaque;
+  private int valorDefensa;
+
+  public HabilidadEspecial (String nombre, int valorAtaque, int valorDefensa) {
+    setNombre(nombre);
+    setValorAtaque(valorAtaque);
+    setValorDefensa(valorDefensa);
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+  public int getValorAtaque() {
+    return valorAtaque;
+  }
+  public void setValorAtaque(int valorAtaque) {
+    if (valorAtaque >= 1 && valorAtaque <= 3) {
+      this.valorAtaque = valorAtaque;
+    } else {
+      System.out.println("Error: Valor de ataque debe estar entre 1 y 3.");
+      this.valorAtaque = 1;
+    }
+  }
+
+  public int getValorDefensa() {
+    return valorDefensa;
+  }
+  public void setValorDefensa(int valorDefensa) {
+    if (valorDefensa >= 1 && valorDefensa <= 3) {
+      this.valorDefensa = valorDefensa;
+    } else {
+      System.out.println("Error: Valor de defensa debe estar entre 1 y 3.");
+      this.valorDefensa = 1;
+    }
+  }
 }
+
