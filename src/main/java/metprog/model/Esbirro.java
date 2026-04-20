509fdebb4eeff437fe73cc2cb12ba24689391998
Sergio Alejandro <sergioromero2k@gmail.com>
2026-04-20 20:48:36 +0200

    Adds an abstract Minion class with attributes, a constructor, and getters and setters

--- src/main/java/metprog/model/Esbirro.java
+++ src/main/java/metprog/model/Esbirro.java
@@ -1,5 +1,31 @@
 package metprog.model;
 
-public class Esbirro {
+import java.io.Serializable;
 
+public abstract class Esbirro implements Serializable {
+  private String nombre;
+  private int salud;
+
+
+  public Esbirro (String nombre, int salud) {
+    setNombre(nombre);
+    setSalud(salud);
+  }
+  public String getNombre() {
+    return nombre;
+  }
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+  public int getSalud() {
+    return salud;
+  }
+  public void setSalud(int salud) {
+    if (salud >= 1 && salud <= 3) {
+      this.salud = salud;
+    } else {
+      System.out.println("Error: Salud debe estar entre 1 y 3.");
+      this.salud = 1;
+    }
+  }
 }
