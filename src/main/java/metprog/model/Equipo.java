1b25a60358b192a17a3b6fa109dc380eeb942001
Sergio Alejandro <sergioromero2k@gmail.com>
2026-04-20 20:54:11 +0200

    Adds an abstract Team class with attributes, a constructor, and getters and setters

--- src/main/java/metprog/model/Equipo.java
+++ src/main/java/metprog/model/Equipo.java
@@ -1,5 +1,44 @@
 package metprog.model;
 
-public class Equipo {
+import java.io.Serializable;
 
+public abstract class Equipo implements Serializable {
+  private String nombre;
+  private int modAtaque;
+  private int modDefensa;
+
+  public Equipo(String nombre, int modAtaque, int modDefensa) {
+    setNombre(nombre);
+    setModAtaque(modAtaque);
+    setModDefensa(modDefensa);
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+  public int getModAtaque() {
+    return modAtaque;
+  }
+  public void setModAtaque(int modAtaque) {
+    if (modAtaque >= 0 && modAtaque <= 3) {
+      this.modAtaque = modAtaque;
+    } else {
+      System.out.println("Error: modAtaque debe estar entre 0 y 3.");
+      this.modAtaque = 0;
+    }
+  }
+  public int getModDefensa() {
+    return modDefensa;
+  }
+  public void setModDefensa(int modDefensa) {
+    if (modDefensa >= 0 && modDefensa <= 3) {
+      this.modDefensa = modDefensa;
+    } else {
+      System.out.println("Error: modDefensa debe estar entre 0 y 3.");
+      this.modDefensa = 0;
+    }
+  }
 }
