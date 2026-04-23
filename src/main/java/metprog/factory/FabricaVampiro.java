b8eac3c2d9ad62f262b3dd2059e177d51b650b1a
Sergio Alejandro <sergioromero2k@gmail.com>
2026-04-23 05:42:40 +0200

    Add FabricaVampiro to Abstract Factory pattern

--- src/main/java/metprog/factory/FabricaVampiro.java
+++ src/main/java/metprog/factory/FabricaVampiro.java
@@ -2,12 +2,25 @@ package metprog.factory;
 
 import metprog.model.*;
 
-public class FabricaVampiro {
+public class FabricaVampiro implements FabricaPersonaje{
   Personaje crearPersonaje(String nombre) {
     Vampiro v = new Vampiro(nombre, 5, 3, 100);
+    v.setPuntosSangre(5);
+    v.setHabilidad(crearHabilidadBase());
+    v.equiparArma(crearArmaInicial());
+    v.añadirArmadura(crearArmaduraInicial());
     return v;
   }
-  Arma crearArmaInicial();
-  Armadura crearArmaduraInicial();
-  HabilidadEspecial crearHabilidadBase();
+  @Override
+  public Arma crearArmaInicial() {
+    return new Arma("Colmillos", 2, 0, false);
+  }
+  @Override
+  public Armadura crearArmaduraInicial() {
+    return new Armadura("Capa Oscura", 0, 2);
+  }
+  @Override
+  public HabilidadEspecial crearHabilidadBase() {
+    return new Disciplina("Dominio Mental", 2, 1, 1);
+  }
 }
--- src/main/java/metprog/model/Vampiro.java
+++ src/main/java/metprog/model/Vampiro.java
@@ -1,4 +1,23 @@
 package metprog.model;
 
-public class Vampiro  {
+public class Vampiro extends Personaje{
+  private int puntosSangre;
+  private int edad;
+
+  public Vampiro(String nombre, int salud, int poder, int oro) {
+    super(nombre, salud, poder, oro);
+    this.puntosSangre = 5;
+    this.edad = 0;
+  }
+  public int getPuntosSangre() { return puntosSangre; }
+  public void setPuntosSangre(int puntosSangre) {
+    if (puntosSangre >= 0 && puntosSangre <= 10) {
+      this.puntosSangre = puntosSangre;
+    } else {
+      System.out.println("Error: PuntosSangre debe estar entre 0 y 10.");
+      this.puntosSangre = 0;
+    }
+  }
+  public int getEdad() {return edad;}
+  public void setEdad(int edad) { this.edad = edad; }
 }
