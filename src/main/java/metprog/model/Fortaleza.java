df51d13cf6ed346f590d27651290f92626b43081
Ngilor <nevillegilor@gmail.com>
2026-04-25 23:50:29 +0200

    EsbirroHumano, Fortaleza included

--- src/main/java/metprog/model/Fortaleza.java
+++ src/main/java/metprog/model/Fortaleza.java
@@ -1,5 +1,13 @@
 package metprog.model;
 
-public class Fortaleza {
+/**
+ * Fortaleza: modificador positivo de un Personaje.
+ * El operador decide qué fortalezas están "presentes" en un combate,
+ * y su valor se suma al potencial de ataque y defensa.
+ */
+public class Fortaleza extends Modificador {
 
+    public Fortaleza(String nombre, int valor) {
+        super(nombre, valor);
+    }
 }
