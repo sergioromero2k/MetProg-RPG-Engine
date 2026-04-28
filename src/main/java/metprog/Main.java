a81407ecc307925d3af05b33041e2aa2192ccd2a
lucca23 <luccamanfredotti@gmail.com>
2026-04-28 13:36:35 +0200

    Realizamos un menu de bienvenida basico

--- src/main/java/metprog/Main.java
+++ src/main/java/metprog/Main.java
@@ -1,6 +1,10 @@
 package metprog;
 
+import metprog.ui.MenuPrincipal;
+
 public class Main {
 	public static void main(String[] args) {
+		MenuPrincipal menuPrincipal = new MenuPrincipal();
+		menuPrincipal.mostrar();
 	}
 }
