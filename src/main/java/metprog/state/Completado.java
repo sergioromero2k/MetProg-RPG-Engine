530c5c12414aaf12cca026a02c2d2b2fc9d33b0c
Sergio Alejandro <sergioromero2k@gmail.com>
2026-05-01 13:27:01 +0200

    añandiend google checks /state/completado.java

--- src/main/java/metprog/state/Completado.java
+++ src/main/java/metprog/state/Completado.java
@@ -2,23 +2,34 @@ package metprog.state;
 
 import metprog.model.Desafio;
 
-public class Completado implements EstadoDesafio{
+/**
+ * Representa el estado final de un desafío que ha concluido con éxito.
+ *
+ * <p>En este estado, todas las acciones de transición son inválidas dado que
+ * el ciclo de vida del desafío ha terminado.
+ */
+public class Completado implements EstadoDesafio {
+
   @Override
   public void validar(Desafio d) {
     System.out.println("El desafío ya está completado.");
   }
+
   @Override
   public void aceptar(Desafio d) {
     System.out.println("El desafío ya está completado.");
   }
+
   @Override
   public void rechazar(Desafio d) {
     System.out.println("El desafío ya está completado.");
   }
+
   @Override
   public void cancelar(Desafio d) {
     System.out.println("El desafío ya está completado.");
   }
+
   @Override
   public void finalizar(Desafio d) {
     System.out.println("No se puede finalizar en este estado.");
